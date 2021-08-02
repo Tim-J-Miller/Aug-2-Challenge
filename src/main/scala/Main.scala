27eba60fb1568fc6ad6a1cf84ee9b1b6d7ebc207
Tim Miller <timothy.miller@revature.net>
2021-08-02 12:09:56 -0400

    Solved Logical Reasoning problem

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,3 +1,51 @@
+/*
+Weekly Coding Challenge:
+
+Problem 1: Given two Strings A and B. Find the length of the Longest Common Subsequence (LCS) of the given Strings. Input: A = Minneapolis, B = Minnesota.
+
+Problem 2: Write java Program to Find Smallest and Largest Element in an Array.
+Note: Number can not be repeated in the array.
+
+For example:
+1
+2
+3
+4
+5
+6
+
+int[] arr1={7,5,6,1,4,2};
+Missing number : 3
+int[] arr2={5,3,1,2};
+Missing number : 4
+
+Logical Reasoning:
+3) This question is based on a certain code language. Understand the logic in the coding and answer the following question.
+
+If PROCESSOR is coded as D4F3C5C1E1S1S1E3C6, what is the code for QUADRANT?
+
+p - d4 - 4*4 - 16
+r - f3 - 6*3 - 18
+o - c5 - 3*5 - 15
+c - c1 - 3*1 - 3
+e - e1 - 5*1 - 5
+s - s1 - 19*1 - 19
+s - s1 - 19*1 - 19
+o - e3 - 5*3 - 15
+r - c6 - 3*6 - 18
+
+q - 17 - 17*1 - q1
+u - 21 - 7*3 - c7
+a - 1 - 1*1 - a1
+d - 4 - 2*2 - b2
+r - 18 - 6*3 - f3
+a - 1 - 1*1 - a1
+n - 14 - 7*2 - c2
+t - 20 - 5*4 - e4
+
+Q1C7A1B2F3A1C2E4
+*/
+
 object Main extends App {
   println("Hello, World!")
 }
